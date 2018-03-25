f7c5b41b0044d7bef6881c48014c31a0e5df082b
Nicholas Dujay <dat2@users.noreply.github.com>
2018-03-25 12:35:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# install vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# setup swap dir for vimrc
 mkdir -p ~/.vim/swapdir
+
+# setup prettier for neoformatter
+npm install -g prettier
