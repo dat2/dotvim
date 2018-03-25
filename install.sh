923e06e9d5d53b862220e015b1f383fae6fc5201
Nicholas Dujay <dat2@users.noreply.github.com>
2018-03-25 14:30:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# install vim plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+# install vim plug (for nvim)
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # setup swap dir for vimrc
