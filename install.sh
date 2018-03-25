9782abd8a8bdd35c8c70e4c14485e3a5e0b8e3bd
Nicholas Dujay <dat2@users.noreply.github.com>
2018-03-25 14:31:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # setup swap dir for vimrc
-mkdir -p ~/.vim/swapdir
+mkdir -p ~/.config/nvim/swapdir
 
 # setup prettier for neoformatter
 npm install -g prettier
